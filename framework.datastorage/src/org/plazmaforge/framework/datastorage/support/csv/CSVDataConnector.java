b14e133051d2ec3ab5d1a9f199f12d46d73fbfd8
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2015-11-27 07:15:49 +0000

    Add stub DataConnectors

---
+++ framework.datastorage/src/org/plazmaforge/framework/datastorage/support/csv/CSVDataConnector.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2012-2015 Oleh Hapon ohapon@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Oleh Hapon
+ * Kyiv, UKRAINE
+ * ohapon@users.sourceforge.net
+ */
+
+/**
+ * 
+ */
+package org.plazmaforge.framework.datastorage.support.csv;
+
+import org.plazmaforge.framework.core.datastorage.AbstractFileDataConnector;
+
+/**
+ * @author ohapon
+ *
+ */
+public class CSVDataConnector extends AbstractFileDataConnector {
+
+    public static final String DEFAULT_FIELD_DELIMETER = ",";
+    public static final String DEFAULT_RECORD_DELIMETER = "\\n";    
+    
+    
+    private String fieldDelimiter = DEFAULT_FIELD_DELIMETER;
+
+    private String recordDelimiter = DEFAULT_RECORD_DELIMETER;
+   
+    private boolean useFirstRowAsHeader;
+
+
+    public CSVDataConnector() {
+	super();
+    }
+    
+    @Override
+    public String getType() {
+	return CSVDataProducerFactory.TYPE;
+    }
+
+    public String getFieldDelimiter() {
+        return fieldDelimiter;
+    }
+
+    public void setFieldDelimiter(String fieldDelimiter) {
+        this.fieldDelimiter = fieldDelimiter;
+    }
+
+    public String getRecordDelimiter() {
+        return recordDelimiter;
+    }
+
+    public void setRecordDelimiter(String recordDelimiter) {
+        this.recordDelimiter = recordDelimiter;
+    }
+
+    public boolean isUseFirstRowAsHeader() {
+        return useFirstRowAsHeader;
+    }
+
+    public void setUseFirstRowAsHeader(boolean useFirstRowAsHeader) {
+        this.useFirstRowAsHeader = useFirstRowAsHeader;
+    }
+
+
+    
+    
+
+}
