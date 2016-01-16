13c37e6c2dfdcc6fde030924789d6e049c1cc779
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2016-01-16 11:13:02 +0000

    Add format tests

--- framework.datastorage/test/org/plazmaforge/framework/datastorage/support/xml/XMLDataTypeTest.java
+++ framework.datastorage/test/org/plazmaforge/framework/datastorage/support/xml/XMLDataTypeTest.java
@@ -27,6 +27,7 @@ package org.plazmaforge.framework.datastorage.support.xml;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 
 import org.plazmaforge.framework.core.datastorage.DSBaseDataSource;
@@ -47,6 +48,9 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    
     
     public void testXMLDataSet() throws Exception {
+
+	// TODO: Temp solution: Use 'locale' attribute to configure locale in DataConnector/DataSet/DataResultSet 
+	Locale.setDefault(Locale.ENGLISH);
 
 	// Data Producer
 	DataProducer producer = new XMLDataProducerFactory().getDataProducer();
@@ -102,6 +106,19 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
 	field.setDataType("DateTime");
 	dataSource.addField(field);
 	
+	field = new DSField();
+	field.setName("f_date_fmt");
+	field.setDataType("Date");
+	field.setFormat("dd/MMM/yyyy"); // DateFormat (Locale)
+	dataSource.addField(field);
+
+	field = new DSField();
+	field.setName("f_time_fmt");
+	field.setDataType("Time");
+	field.setFormat("hh:mm a"); // TimeFormat
+	dataSource.addField(field);
+	
+	
 	DSDataSet dataSet = producer.openDataSet(session, dataSource);
 	
 	int row = 0;
@@ -117,6 +134,8 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    	Date valueDate = null;
    	Date valueTime = null;
    	Date valueDateTime = null;
+   	Date valueDateFmt = null;
+   	Date valueTimeFmt = null;
    	
    	while (dataSet.next()) {
    	    
@@ -126,6 +145,9 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    	    valueDate = (Date) dataSet.getValue("f_date");
    	    valueTime = (Date) dataSet.getValue("f_time");
    	    valueDateTime = (Date) dataSet.getValue("f_date_time");
+   	    valueDateFmt = (Date) dataSet.getValue("f_date_fmt");
+   	    valueTimeFmt = (Date) dataSet.getValue("f_time_fmt");
+   	    
    	    
    	    if (row == 0) {
    		assertEquals(valueString, "String 1");
@@ -134,6 +156,8 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    		assertEquals(valueDate, getDate(2001, 1, 11));
    		assertEquals(valueTime, getTime(11, 1, 21));
    		assertEquals(valueDateTime, getDateTime(2011, 1, 11, 21, 1, 11));
+   		assertEquals(valueDateFmt, getDate(1951, 1, 21));
+   		assertEquals(valueTimeFmt, getTime(1, 21, 00));
    	    } else if (row == 1) {
    		assertEquals(valueString, "String 2");
    		assertEquals(valueInteger, new Integer(200));
@@ -141,6 +165,8 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    		assertEquals(valueDate, getDate(2002, 2, 12));
    		assertEquals(valueTime, getTime(12, 2, 22));
    		assertEquals(valueDateTime, getDateTime(2012, 2, 12, 22, 2, 12));
+   		assertEquals(valueDateFmt, getDate(1952, 2, 22));
+   		assertEquals(valueTimeFmt, getTime(14, 22, 00));
    	    } else if (row == 2) {
    		assertEquals(valueString, "String 3");
    		assertEquals(valueInteger, new Integer(300));
@@ -148,6 +174,8 @@ public class XMLDataTypeTest extends AbstractDSTestCase {
    		assertEquals(valueDate, getDate(2003, 3, 13));
    		assertEquals(valueTime, getTime(13, 3, 23));
    		assertEquals(valueDateTime, getDateTime(2013, 3, 13, 23, 3, 13));
+   		assertEquals(valueDateFmt, getDate(1953, 3, 23));
+   		assertEquals(valueTimeFmt, getTime(3, 23, 00));
    	    }
 
    	    System.out.println(" Row[" + row + "] : " 
