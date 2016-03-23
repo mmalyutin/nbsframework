57494bdbf10ded761e3a6c53791c6a500f935df0
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2016-03-23 09:24:13 +0000

    Refactored: validation -> data.validator

--- framework.core/src/org/plazmaforge/framework/core/data/validator/ContainerValidator.java
+++ framework.core/src/org/plazmaforge/framework/core/data/validator/ContainerValidator.java
@@ -1,4 +1,4 @@
-package org.plazmaforge.framework.core.validation;
+package org.plazmaforge.framework.core.data.validator;
 
 import java.util.ArrayList;
 import java.util.List;
--- framework.core/src/org/plazmaforge/framework/core/data/validator/type/NotNull.java
+++ framework.core/src/org/plazmaforge/framework/core/data/validator/type/NotNull.java
@@ -1,6 +1,12 @@
-package org.plazmaforge.framework.core.validation;
+package org.plazmaforge.framework.core.data.validator.type;
 
 import java.util.List;
+
+import org.plazmaforge.framework.core.data.validator.AbstractValidator;
+import org.plazmaforge.framework.core.data.validator.ExpressionValidator;
+import org.plazmaforge.framework.core.data.validator.Message;
+import org.plazmaforge.framework.core.data.validator.ValidationResult;
+import org.plazmaforge.framework.core.data.validator.Validator;
 
 public class NotNull extends AbstractValidator implements ExpressionValidator {
 
