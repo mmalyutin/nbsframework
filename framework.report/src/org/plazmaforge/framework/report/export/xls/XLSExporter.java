c2ef23305c04e2c7bdc18a0b89d75badfbb354ec
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2016-09-29 12:39:46 +0000

    Refactored

--- framework.report/src/org/plazmaforge/framework/report/export/xls/XLSExporter.java
+++ framework.report/src/org/plazmaforge/framework/report/export/xls/XLSExporter.java
@@ -22,187 +22,32 @@
 
 package org.plazmaforge.framework.report.export.xls;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFPalette;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.plazmaforge.framework.report.export.AbstractWorkbookExporter;
-import org.plazmaforge.framework.report.model.base.grid.Cell;
-import org.plazmaforge.framework.uwt.graphics.Color;
-import org.plazmaforge.framework.uwt.graphics.Font;
+import org.plazmaforge.framework.report.export.AbstractXLWorkbookExporter;
 
 /**
  * 
  * @author ohapon
  *
  */
-public class XLSExporter extends AbstractWorkbookExporter {
+public class XLSExporter extends AbstractXLWorkbookExporter {
 
-    protected HSSFWorkbook xWorkbook;
-
-    protected HSSFSheet xSheet;
-
-    protected HSSFRow xRow;
-
-    protected HSSFCell xCell;
-
-    protected HSSFCellStyle emptyCellStyle;
-
-    protected HSSFCellStyle cellStyle;
     
-    protected Map<String, HSSFColor> colorMap;
-    
-    protected Map<String, HSSFFont> fontMap;
-    
-    
-  
-   
-    
-    @Override
-    protected void initExport() {
-	
-	super.initExport();
-	
-	xWorkbook = null;
-	xSheet = null;
-	xRow = null;
-	xCell = null;
-	emptyCellStyle = null;
-	cellStyle = null;
-	
-	colorMap = new HashMap<String, HSSFColor>();
-	fontMap = new HashMap<String, HSSFFont>();
-    }
-   
     @Override
     protected void createXWorkbook() {
 	xWorkbook = new HSSFWorkbook();
 	
-	HSSFCellStyle emptyCellStyle = xWorkbook.createCellStyle();
-	emptyCellStyle.setFillForegroundColor((new HSSFColor.WHITE()).getIndex());
-	
-	// emptyCellStyle.setFillPattern(backgroundMode);
-	// dataFormat = workbook.createDataFormat();
+	emptyCellStyle = xWorkbook.createCellStyle();
+	//emptyCellStyle.setFillForegroundColor((new HSSFColor.WHITE()).getIndex());
+	//emptyCellStyle.setFillPattern(backgroundMode);
+	//dataFormat = workbook.createDataFormat();
     }
-    
-    @Override
-    protected void writeXWorkbook(OutputStream os) throws IOException {
-	xWorkbook.write(os);
-    }
-    
-    @Override
-    protected void createXSheet(String name) {
-	xSheet = xWorkbook.createSheet(name);
-    }
-    
-    @Override
-    protected void createXRow(int rowIndex) {
-	xRow = xSheet.createRow(rowIndex);
-    }
-    
-    @Override
-    protected void createXCell(int columnIndex){
-	xCell = xRow.createCell((short) columnIndex);
-    }
-    
-    @Override
-    protected void setXCellStyle(Cell cell) {
-	if (background == null && foreground == null && font == null) {
-	    return;
-	}
-	
-	cellStyle = xWorkbook.createCellStyle();
-
-	// cell: background
-	if (background != null) {
-	    HSSFColor xColor = getXColor(background);
-	    if (xColor != null) {
-		cellStyle.setFillForegroundColor(xColor.getIndex());
-		cellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
-	    }
-	}
-
-	// cell: foreground, font
-	if (foreground != null || font != null) {
-	    HSSFFont xFont = getXFont(font, foreground);
-	    if (xFont != null) {
-		cellStyle.setFont(xFont);
-	    }
-	}
-
-	xCell.setCellStyle(cellStyle);
-
-    }
-    
-    @Override
-    protected void setXCellSize(Cell cell) {
-	if (colspan > 1 || rowspan > 1) {
-	    // TODO
-	    xSheet.addMergedRegion(new CellRangeAddress(rowIndex, (rowIndex + rowspan - 1), columnIndex, (columnIndex + colspan - 1)));
-	}
-    }
-    
-    @Override
-    protected void setXCellValue(Cell cell) {
-	Object value = cell.getValue();
-	if (value != null) {
-	    xCell.setCellValue(value.toString());
-	}
-    }
-    
-    @Override
-    protected void setXColumnWidth(int columnIndex, int columnWidth) {
-	xSheet.setColumnWidth(columnIndex, columnWidth * 43); // Math.min(43 * width, 256*255));
-    }
-    
-    @Override
-    protected void setXRowHeight(int height) {
-	xRow.setHeightInPoints(height);
-    }
-    
  
-    ////
-
-    protected HSSFSheet createXSheet(HSSFWorkbook workbook, String name) {
-	return workbook.createSheet(name);
-    }
-
-    protected HSSFColor getXColor(Color color) {
-	if (color == null) {
-	    return null;
-	}
-	HSSFColor xColor = colorMap.get(color.getKey());
-	if (xColor != null) {
-	    return xColor;
-	}
-	xColor = createXColor(color);
-	if (xColor == null) {
-	    return null;
-	}
-	colorMap.put(color.getKey(), xColor);
-	return xColor;
-    }
-    
-    protected HSSFColor createXColor(Color color) {
-	if (color == null) {
-	    return null;
-	}
-	return createXColor((byte) color.getRed(), (byte) color.getGreen(), (byte) color.getBlue());
-    }
-    
-    protected HSSFColor createXColor(byte r, byte g, byte b) {
-	HSSFPalette palette = xWorkbook.getCustomPalette();
+    protected org.apache.poi.ss.usermodel.Color createXColor(byte r, byte g, byte b) {
+	HSSFPalette palette = ((HSSFWorkbook) xWorkbook).getCustomPalette();
 	HSSFColor hssfColor = null;
 	try {
 	    hssfColor = palette.findColor(r, g, b);
@@ -213,62 +58,19 @@ public class XLSExporter extends AbstractWorkbookExporter {
 	} catch (Exception e) {
 	    //logger.error(e);
 	}
-
 	return hssfColor;
     }
     
-    protected HSSFFont getXFont(Font font, Color color) {
-	String key = getFontColorKey(font, color);
-	if (key == null) {
-	    return null;
-	}
-	HSSFFont xFont = fontMap.get(key);
-	if (xFont != null) {
-	    return xFont;
-	}
-	xFont = createXFont(font, color);
-	if (xFont == null) {
-	    return xFont;
-	}
-	fontMap.put(key, xFont);
-	return xFont;
+    protected void setXFontColor(org.apache.poi.ss.usermodel.Font font, org.apache.poi.ss.usermodel.Color color) {
+	font.setColor(((HSSFColor) color).getIndex());
     }
-    
-    protected HSSFFont createXFont(Font font, Color color) {
-	if (font == null && color == null) {
-	    return null;
-	}
-	HSSFFont xFont = xWorkbook.createFont();
-	if (font != null) {
-	    if (font.getName() != null) {
-		xFont.setFontName(font.getName());
-	    }
-	    if (font.getSize() > 0) {
-		xFont.setFontHeightInPoints((short) font.getSize());
-	    }
-	    if (font.isBold()) {
-		xFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-	    }
-	    if (font.isItalic()) {
-		xFont.setItalic(true);
-	    }
-	    /*
-	    if (font.isUnderline()) {
-		cellFont.setUnderline(HSSFFont.U_SINGLE);
-	    }
-	    if (font.isStrikeThrough()) {
-		cellFont.setStrikeout(true);
-	    }
-	    */
-	}
-	if (color != null) {
-	    HSSFColor xColor = getXColor(color);
-	    if (xColor != null) {
-		xFont.setColor(xColor.getIndex());
-	    }
-	}
-	return xFont;
+
+    protected void setXCellStyleFillForeground(org.apache.poi.ss.usermodel.CellStyle cellStyle, org.apache.poi.ss.usermodel.Color color) {
+	cellStyle.setFillForegroundColor(((HSSFColor) color).getIndex());
     }
-    
+
+    protected void setXCellStyleFillBackground(org.apache.poi.ss.usermodel.CellStyle cellStyle, org.apache.poi.ss.usermodel.Color color) {
+	cellStyle.setFillBackgroundColor(((HSSFColor) color).getIndex());
+    }
     
 }
