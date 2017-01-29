815c09bf3c53cf4ac338a209795f109b0859d1de
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-01-29 09:11:20 +0000

    Add stub PDF Exporter

---
+++ framework.report/src/org/plazmaforge/framework/report/export/pdf/PDFExporter.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2012-2015 Oleh Hapon ohapon@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Oleh Hapon
+ * Kyiv, UKRAINE
+ * ohapon@users.sourceforge.net
+ */
+
+/**
+ * 
+ */
+package org.plazmaforge.framework.report.export.pdf;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.plazmaforge.framework.report.exception.RTException;
+import org.plazmaforge.framework.report.export.AbstractBaseExporter;
+import org.plazmaforge.framework.report.model.document.Document;
+import org.plazmaforge.framework.report.model.document.Page;
+
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfWriter;
+
+/**
+ * @author ohapon
+ *
+ */
+public class PDFExporter extends AbstractBaseExporter {
+
+    protected PdfWriter writer;
+    protected com.lowagie.text.Document pdfDocument;
+    
+    @Override
+    public void exportDocument(Document document) throws RTException {
+	try {
+	    ensureOutput();
+	    checkOutput();
+
+	    writeDocument(document);
+
+	    flushOutput();
+	    closeOutput();
+	    
+	} catch (Exception ex) {
+	    throw new RTException(ex);
+	}
+    }
+
+    @Override
+    public void exportPage(Page page) throws RTException {
+	// TODO Auto-generated method stub
+    }
+    
+    protected void ensureOutput() throws RTException {
+
+	String outputType = getOutputType();
+	if (!isValidOutputType(outputType)) {
+	    throw new RTException("Invalid output type: '" + outputType + "'");
+	}
+	
+	String encoding = null; //getEncoding();
+	if (isFileNameOutputType(outputType)) {
+	    String fileName = (String) getData(PROPERTY_OUTPUT_FILE_NAME);
+	    ensureOutput(fileName, encoding);
+	} else {
+	    //TODO
+	    throw new RTException("Invalid output type: '" + outputType + "'");
+	}
+    }
+   
+    protected void ensureOutput(String fileName, String encoding) throws RTException {
+	if (fileName == null) {
+	    throw new RTException("Output file name is empty");
+	}
+	try {
+	    
+	   pdfDocument = new com.lowagie.text.Document(
+				new Rectangle(
+					500,
+					300
+				)
+			);
+	    
+	    FileOutputStream os = new FileOutputStream(fileName);
+	    writer = PdfWriter.getInstance(pdfDocument, os);
+	    writer.setCloseStream(false);
+	   
+	    pdfDocument.open();
+	    //pdfDocument.add(new Paragraph("Some content here"));
+	    
+	    //writer = createWriter(fileName, encoding);	    
+	} catch (Exception ex){
+	    throw new RTException(ex);
+	}
+    }
+    
+    protected void checkOutput() throws RTException {
+	if (writer == null) {
+	    throw new RTException("Output is not ready");
+	}
+    }
+
+    protected void flushOutput() throws RTException {
+	//try {
+	//    writer.flush();
+	//} catch (IOException ex) {
+	//    throw new RTException(ex);
+	//}
+    }
+
+    protected void closeOutput() throws RTException {
+	//try {
+	pdfDocument.close();
+	    writer.close();
+	//} catch (IOException ex) {
+	//    throw new RTException(ex);
+	//}
+    }
+    
+    protected void writeDocument(Document document) throws RTException, IOException, DocumentException {
+   	if (document == null || !document.hasPages()) {
+   	    return;
+   	}
+   	
+   	int pageCount = document.getPageCount();
+   	List<Page> pages = document.getPages();
+
+   	//pageOffsetX = offsetX;
+   	//pageOffsetY = offsetY;
+   	
+   	//level = -1;
+   	
+
+   	for (int pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+   	    Page page = pages.get(pageIndex);
+   	    writePage(page, pageIndex);
+   	}
+   	
+       }
+
+       protected void writePage(Page page, int pageIndex) throws RTException, IOException, DocumentException {
+   	//offsetX = 0;
+   	//offsetY = 0;
+	   pdfDocument.newPage();
+	   pdfDocument.add(new Paragraph("Page " + (pageIndex + 1)));
+       }
+       
+    ////
+    
+    
+
+}
