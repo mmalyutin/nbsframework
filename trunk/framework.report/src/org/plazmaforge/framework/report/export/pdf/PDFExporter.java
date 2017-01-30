ec54f60d4937022411c62c3f5cb2435cc52fe351
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-01-30 11:01:39 +0000

    Implemented simple PDF export

--- trunk/framework.report/src/org/plazmaforge/framework/report/export/pdf/PDFExporter.java
+++ trunk/framework.report/src/org/plazmaforge/framework/report/export/pdf/PDFExporter.java
@@ -31,12 +31,32 @@ import java.util.List;
 
 import org.plazmaforge.framework.report.exception.RTException;
 import org.plazmaforge.framework.report.export.AbstractBaseExporter;
+import org.plazmaforge.framework.report.export.html.Attributes;
+import org.plazmaforge.framework.report.model.base.Border;
+import org.plazmaforge.framework.report.model.base.BorderRegion;
+import org.plazmaforge.framework.report.model.base.Element;
+import org.plazmaforge.framework.report.model.base.Insets;
+import org.plazmaforge.framework.report.model.base.Margin;
+import org.plazmaforge.framework.report.model.base.PageSetup;
+import org.plazmaforge.framework.report.model.base.Size;
+import org.plazmaforge.framework.report.model.base.grid.Cell;
+import org.plazmaforge.framework.report.model.base.grid.Column;
+import org.plazmaforge.framework.report.model.base.grid.Grid;
+import org.plazmaforge.framework.report.model.base.grid.GridLayout;
+import org.plazmaforge.framework.report.model.base.grid.GridUtils;
+import org.plazmaforge.framework.report.model.base.grid.Row;
 import org.plazmaforge.framework.report.model.document.Document;
 import org.plazmaforge.framework.report.model.document.Page;
+import org.plazmaforge.framework.uwt.graphics.Color;
+import org.plazmaforge.framework.uwt.graphics.Font;
 
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfCell;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
@@ -44,6 +64,10 @@ import com.lowagie.text.pdf.PdfWriter;
  *
  */
 public class PDFExporter extends AbstractBaseExporter {
+    
+    protected int offsetX;
+    protected int offsetY;
+
 
     protected PdfWriter writer;
     protected com.lowagie.text.Document pdfDocument;
@@ -86,28 +110,21 @@ public class PDFExporter extends AbstractBaseExporter {
 	}
     }
    
-    protected void ensureOutput(String fileName, String encoding) throws RTException {
+    protected void ensureOutput(String fileName, String encoding)
+	    throws RTException {
 	if (fileName == null) {
 	    throw new RTException("Output file name is empty");
 	}
 	try {
-	    
-	   pdfDocument = new com.lowagie.text.Document(
-				new Rectangle(
-					500,
-					300
-				)
-			);
-	    
+
 	    FileOutputStream os = new FileOutputStream(fileName);
+	    pdfDocument = new com.lowagie.text.Document();
+
 	    writer = PdfWriter.getInstance(pdfDocument, os);
 	    writer.setCloseStream(false);
-	   
+
 	    pdfDocument.open();
-	    //pdfDocument.add(new Paragraph("Some content here"));
-	    
-	    //writer = createWriter(fileName, encoding);	    
-	} catch (Exception ex){
+	} catch (Exception ex) {
 	    throw new RTException(ex);
 	}
     }
@@ -129,40 +146,483 @@ public class PDFExporter extends AbstractBaseExporter {
     protected void closeOutput() throws RTException {
 	//try {
 	pdfDocument.close();
-	    writer.close();
+	writer.close();
 	//} catch (IOException ex) {
 	//    throw new RTException(ex);
 	//}
     }
     
     protected void writeDocument(Document document) throws RTException, IOException, DocumentException {
-   	if (document == null || !document.hasPages()) {
+	if (document == null || !document.hasPages()) {
+	    return;
+	}
+
+	int pageCount = document.getPageCount();
+	List<Page> pages = document.getPages();
+	
+	PageSetup pageSetup = document.getPage(0).getPageSetup();
+	Size pageSize = pageSetup.getSize();
+	Margin pageMargin = pageSetup.getMargin();
+	
+	pdfDocument.setPageSize(new Rectangle(pageSize.getWidth(), pageSize.getHeight()));
+	pdfDocument.setMargins(pageMargin.getLeft(), pageMargin.getTop(), pageMargin.getRight(), pageMargin.getBottom());
+
+	// pageOffsetX = offsetX;
+	// pageOffsetY = offsetY;
+
+	// level = -1;
+
+	for (int pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+	    Page page = pages.get(pageIndex);
+	    writePage(page, pageIndex);
+	}
+
+    }
+
+    protected void writePage(Page page, int pageIndex) throws RTException, IOException, DocumentException {
+	// offsetX = 0;
+	// offsetY = 0;
+	pdfDocument.newPage();
+	//pdfDocument.add(new Paragraph("Page " + (pageIndex + 1)));
+	
+	if (!page.hasChildren()) {
+	    return;
+	}
+
+	List<Element> children = page.getChildren();
+
+	for (Element element : children) {
+	    if (element instanceof Grid) {
+		writeGrid((Grid) element);
+	    }
+	}
+    }
+       
+    
+    protected void writeGrid(Grid grid) throws RTException, IOException, DocumentException {
+   	if (!grid.hasRows()) {
    	    return;
    	}
    	
-   	int pageCount = document.getPageCount();
-   	List<Page> pages = document.getPages();
-
-   	//pageOffsetX = offsetX;
-   	//pageOffsetY = offsetY;
+   	int columnCount = grid.getColumnCount();
+   	int rowCount = grid.getRowCount();
    	
-   	//level = -1;
+   	List<Column> columns = grid.getColumns();
+   	List<Row> rows = grid.getRows();
+   	
+   	GridLayout layout = GridUtils.getGridLayout(grid);
+   	
+   	// Get grid size without grid border
+   	int gridWidth = layout.getAreaWidth();
+   	int gridHeight = layout.getAreaHeight();
+
+   	//int gridOffsetX = offsetX;
+   	//int gridOffsetY = offsetY;
+   	
+   	int columnIndex = 0;
+   	int rowIndex = 0;
+   	int cellIndex = 0;
+   	
+   	Row row = null;
+   	Cell cell = null;
+   	
+   	int rowOffsetX = 0;
+   	int rowOffsetY = 0;
+   	int rowWidth = 0;
+   	int rowHeight = 0;
+
+   	// !NEW!
+   	//rowOffsetX = gridOffsetX;
+   	//rowOffsetY = gridOffsetY;
+
+   	int columnWidth = 0;
+   	
+   	// TODO:GC
+   	Color contextBackground = null ;// gc.getBackground();
+   	Color contextForeground = null; //gc.getForeground();
+   	Font contextFont = null; //gc.getFont();
+   	// TODO:GC
+
+   	// Current parent: NOT NULL
+   	parentBackground = null;
+   	parentForeground = null;
+   	parentFont = null;
+   	
+   	// Current: NOT NULL
+   	background = null;
+   	foreground = null;
+   	font = null;
+
+   	
+   	Color gridBackground = null;
+   	Color gridForeground = null;
+   	Font gridFont = null;
+   	
+   	Color rowBackground = null;
+   	Color rowForeground = null;
+   	Font rowFont = null;
+
+   	Color cellBackground = null;
+   	Color cellForeground = null;
+   	Font cellFont = null;
    	
 
-   	for (int pageIndex = 0; pageIndex < pageCount; pageIndex++) {
-   	    Page page = pages.get(pageIndex);
-   	    writePage(page, pageIndex);
-   	}
+   	// grid: parent gc
+   	parentBackground = contextBackground;
+   	parentForeground = contextForeground;
+   	parentFont = contextFont;
    	
-       }
+   	// grid: load gc
+   	gridBackground = grid.getBackground();
+   	gridForeground = grid.getForeground();
+   	gridFont = grid.getFont();
 
-       protected void writePage(Page page, int pageIndex) throws RTException, IOException, DocumentException {
-   	//offsetX = 0;
-   	//offsetY = 0;
-	   pdfDocument.newPage();
-	   pdfDocument.add(new Paragraph("Page " + (pageIndex + 1)));
-       }
-       
+   	// grid: current gc
+   	background = gridBackground;
+   	foreground = gridForeground;
+   	font = gridFont;
+   	
+
+   	PdfPTable table = new PdfPTable(columnCount); 
+   	float[] columnWidths = new float[columnCount];
+   	
+	for (int i = 0; i < columnCount; i++) {
+	    columnIndex = i;
+	    columnWidths[columnIndex] = columns.get(columnIndex).getWidth();
+	}
+	
+	table.setWidths(columnWidths);
+	
+	
+	for (int i = 0; i < rowCount; i++) {
+
+	    row = rows.get(i);
+
+	    // !NEW!
+	    //rowOffsetX = gridOffsetX;
+	    // rowOffsetY = offsetY;
+
+	    rowWidth = gridWidth;
+	    rowHeight = row.getHeight();
+	    
+	    int rowBorderTop = 0;
+   	    int rowBorderBottom = 0;
+   	    BorderRegion rowBorder = layout.getRowBorder(i);
+   	    if (rowBorder != null) {
+   		rowBorderTop = rowBorder.getPrevWidth();
+   		rowBorderBottom = rowBorder.getNextWidth();
+   	    }
+   	    
+   	    // row: parent gc
+   	    parentBackground = getColor(gridBackground, contextBackground);
+   	    parentForeground = getColor(gridForeground, contextForeground);
+   	    parentFont = getFont(gridFont, contextFont);
+   		
+   	    // row: load gc
+   	    rowBackground = row.getBackground();
+   	    rowForeground = row.getForeground();
+   	    rowFont = row.getFont();
+
+   	    // row: current gc
+   	    background = rowBackground;
+   	    foreground = rowForeground;
+   	    font = rowFont;
+   		
+   	    // row: start (position, size, background)
+   	    //table.add
+   	    /*
+   	    styleAttributes = new Attributes();
+   	    
+   	    if (isTableAsDiv()) {
+   		setPosition(styleAttributes, rowOffsetX, rowOffsetY);
+   		setSize(styleAttributes, rowWidth, rowHeight);
+   	    } else {
+   		setHeight(styleAttributes, rowHeight);
+   	    }
+   	    
+   	    setBackground(styleAttributes, background);
+   	    setForeground(styleAttributes, foreground);
+   	    setFont(styleAttributes, font);
+   	 
+   	    style = styleAttributes.toStyleAttribute("style");
+   	    
+   	    writeRowStart(style);
+   	    */
+   	    
+   	    // row: normalize current gc
+   	    normalizeCurrentStyle();
+   	    
+   	    // row: init gc
+   	    // TODO:GC
+   	    //setCurrentStyle(gc);
+   	    
+   	    columnIndex = 0;
+   	    //rowIndex = 0;
+   	    
+   	    int cellX = rowOffsetX;
+   	    int cellY = 0; // !NEW!
+   	    //int cellY = rowOffsetY;
+   	    
+   	    int cellWidth = 0;
+   	    int cellHeight = 0;
+   	    int colspan = 0;
+   	    int rowspan = 0;
+   	    
+   	    int paddingLeft = 0;
+   	    int paddingTop = 0;
+   	    int paddingRight = 0;
+   	    int paddingBottom = 0;
+   	    
+   	    int cellCount = row.getCellCount();
+    	    List<Cell> cells = row.getCells();
+   	    
+    	    // shift cellY (row-border-top)
+    	    cellY += rowBorderTop;	
+    	    
+    	    
+    	    
+    	    
+   	    for (int j = 0; j < cellCount; j++) {
+   		
+   		cellIndex = j;
+   		cell = cells.get(cellIndex);
+   		
+   		cellWidth = 0;
+   		cellHeight = 0;
+   		colspan = cell.getColspan();
+   		rowspan = cell.getRowspan();
+   		int nextColumnIndex = columnIndex + colspan;
+   		int nextRowIndex = rowIndex + rowspan;
+   		
+   		if (nextColumnIndex > columnCount) {
+   		    // overflow columns
+   		    break;
+   		}
+   		if (nextRowIndex > rowCount) {
+   		    // overflow rows
+   		    break;
+   		}
+   		
+   		columnWidth = columns.get(columnIndex).getWidth();
+   		//int rowHeight
+   		
+   		cellWidth = GridUtils.calculateCellWidth(layout, cell, columns, columnIndex);
+   		cellHeight = GridUtils.calculateCellHeight(layout, cell, rows, rowIndex);
+   		
+   		int columnBorderLeft = 0;
+   		int columnBorderRight = 0;
+   		
+   		BorderRegion columnBorder1 = layout.getColumnBorder(columnIndex);
+   		if (columnBorder1 != null) {
+   		    columnBorderLeft = columnBorder1.getPrevWidth();
+   		}
+   		BorderRegion columnBorder2 = layout.getColumnBorder(nextColumnIndex - 1);
+   		if (columnBorder2 != null) {
+   		    columnBorderRight = columnBorder2.getNextWidth();
+   		}
+
+   		// shift cellX (column-border-left)
+   		cellX += columnBorderLeft;		  
+   		
+   		// cell: parent gc
+   		parentBackground = getColor(rowBackground, gridBackground != null ? gridBackground : contextBackground);
+   		parentForeground = getColor(rowForeground, gridForeground != null ? gridForeground : contextForeground);
+   		parentFont = getFont(rowFont, gridFont != null ? gridFont: contextFont);
+   		
+   		// cell: load gc
+   		cellBackground = cell.getBackground();
+   		cellForeground = cell.getForeground();
+   		cellFont = cell.getFont();
+   		
+   		// cell: current gc
+   		background = cellBackground;
+   		foreground = cellForeground;
+   		font = cellFont;
+
+   		// fill outer cell
+   		int fillCellX = cellX - columnBorderLeft;
+   		int fillCellY = cellY - rowBorderTop;
+   		int fillCellWidth = cellWidth  + columnBorderLeft + columnBorderRight;
+   		int fillCellHeight = cellHeight + rowBorderTop + rowBorderBottom;
+   		
+   		Border border = layout.getCellBorder(columnIndex, rowIndex);
+
+   		int borderLeft = 0;
+   		int borderRight = 0;
+   		int borderTop = 0;
+   		int borderBottom = 0;
+
+   		if (border != null && !border.isEmpty()) {
+    		   borderLeft = getLineWidth(border.hasLeft() ? border.getLeft() : null);
+ 		   borderRight = getLineWidth(border.hasRight() ? border.getRight() : null);
+ 		   borderTop = getLineWidth(border.hasTop() ? border.getTop() : null);
+ 		   borderBottom = getLineWidth(border.hasBottom() ? border.getBottom() : null);
+ 		   
+ 		   // fixed size
+ 		   fillCellWidth -= (borderLeft + borderRight);
+ 		   fillCellHeight -= (borderTop + borderBottom);   		
+   		}
+   		
+   		
+   		// cell: padding
+   		paddingLeft = 0;
+   		paddingTop = 0;
+   		paddingRight = 0;
+   		paddingBottom = 0;
+
+   		if (cell.hasPadding()) {
+   		    paddingLeft = cell.getPadding().getLeft();
+   		    paddingTop = cell.getPadding().getTop();
+   		    paddingRight = cell.getPadding().getRight();
+   		    paddingBottom = cell.getPadding().getBottom();
+   		}
+   		
+   		
+   		// cell: start (position, size, background, foreground, border)
+   		
+   		
+   		/*
+   		styleAttributes = new Attributes();
+   		
+   		String attr = null;
+   		
+   		    
+   		    // cell: [TABLE:TD] position, size = none, use first empty row in table (invisible: height=0)
+   		    //if (rowIndex == 0) {
+   			//setWidth(styleAttributes, columnWidth);
+   		    //}
+   		    
+   		    // cell: [TABLE:TD] padding
+   		    if (cell.hasPadding()) {
+   			setPadding(styleAttributes, cell.getPadding());
+   		    }
+   		    
+   		    if (colspan > 1) {
+   			attr = "colspan=\"" + colspan + "\"";
+   		    }
+   		    if (rowspan > 1) {
+   			if (attr == null) {
+   			    attr = "";
+   			} else {
+   			    attr = attr + " ";
+   			}
+   			attr += "rowspan=\"" + rowspan + "\"";
+   		    }
+   		
+   		
+   		setBorder(styleAttributes, border);
+   		setBackground(styleAttributes, background);
+   		setForeground(styleAttributes, foreground);
+   		setFont(styleAttributes, font);
+   		
+   		style = styleAttributes.toStyleAttribute("style");
+   		if (attr != null) {
+   		    style += (" " + attr);
+   		}
+   		
+   		writeCellStart(style);
+   		*/
+   		
+   		// cell: normalize current gc
+   		normalizeCurrentStyle();
+   		
+   		// cell: init gc
+   		//setCurrentStyle(gc);
+   		
+   		
+   		// cell: area
+   		int areaX = /*cellX + */ paddingLeft; // #REL
+   		int areaY = /*cellY + */ paddingTop;  // #REL
+   		int areaWidth = cellWidth - paddingLeft - paddingRight;
+   		int areaHeight = cellHeight - paddingTop - paddingBottom;
+   		
+   		// cell: paint
+   		boolean isTextPosition = false; //isTableAsDiv();
+   		Insets textMargin = null;
+   		
+   		// cell: [TABLE:TD] textMargin
+   		//if (isTableAsDiv()) {
+   		//    textMargin =  cell.hasPadding() ? cell.getPadding() : null;
+   		//}
+   		
+   		String text = null;
+   		if (areaWidth > 0 && areaHeight > 0) {
+   		    Object value = cell.getValue();
+   		    if (value != null) {
+   			text = formatCellValue(cell);
+   			
+   			//writeText(text, (isTextPosition ? areaX : null), (isTextPosition ? areaY : null), areaWidth, areaHeight, textMargin, font, foreground, cell.getHorizontalAlign(), cell.getVerticalAlign());
+   		    }
+   		}
+   		if (text == null) {
+   		    text = "";
+   		}
+   		
+		PdfPCell pdfCell = new PdfPCell(new Phrase(text));
+		pdfCell.setFixedHeight(cellHeight);
+		
+		if (colspan > 1) {
+		    pdfCell.setColspan(colspan);
+		}
+		if (rowspan > 1) {
+		    pdfCell.setRowspan(rowspan);
+		}
+   		table.addCell(pdfCell);
+   		
+   		// cell: end
+   	   	    
+   		columnIndex = nextColumnIndex;
+   		cellX += cellWidth;
+   		//cellX += columnBorderLeft;
+   		cellX += columnBorderRight;
+   		
+   	    }
+   	    
+   	    // row: end
+
+   	    rowIndex++;
+   	    int shiftY = row.getHeight() + rowBorderTop + rowBorderBottom;
+   	    rowOffsetY += shiftY;
+   	    offsetY += shiftY;
+   	 
+   	    //offsetY += row.getHeight();
+   	    //offsetY += rowBorderTop;
+   	    //offsetY += rowBorderBottom;
+   	    
+   	    // row: parent gc
+   	    parentBackground = getColor(gridBackground, contextBackground);
+   	    parentForeground = getColor(gridForeground, contextForeground);
+   	    parentFont = getFont(gridFont, contextFont);
+   		
+   	    // row: load gc
+   	    rowBackground = row.getBackground();
+   	    rowForeground = row.getForeground();
+   	    rowFont = row.getFont();
+
+   	    // row: current gc
+   	    background = rowBackground;
+   	    foreground = rowForeground;
+   	    font = rowFont;
+   	    
+   	    // row: fix current gc
+   	    normalizeCurrentStyle();
+   	    
+   	    // row: init gc
+   	    // TODO:GC
+   	    //setCurrentStyle(gc);
+
+   	    
+   	    
+   	    
+   	    
+	}
+	
+	
+	pdfDocument.add(table);
+	
+    }
+    
+    
     ////
     
     
