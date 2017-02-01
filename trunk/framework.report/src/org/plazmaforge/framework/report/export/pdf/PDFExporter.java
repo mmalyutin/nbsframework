2538c0a11de62b4154943b5ccfcda699d2a00908
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-02-01 14:49:41 +0000

    Refactored

--- trunk/framework.report/src/org/plazmaforge/framework/report/export/pdf/PDFExporter.java
+++ trunk/framework.report/src/org/plazmaforge/framework/report/export/pdf/PDFExporter.java
@@ -433,9 +433,9 @@ public class PDFExporter extends AbstractBaseExporter {
    		cellX += columnBorderLeft;		  
    		
    		// cell: parent gc
-   		parentBackground = getColor(rowBackground, gridBackground != null ? gridBackground : contextBackground);
-   		parentForeground = getColor(rowForeground, gridForeground != null ? gridForeground : contextForeground);
-   		parentFont = getFont(rowFont, gridFont != null ? gridFont: contextFont);
+   		parentBackground = getColor(rowBackground, gridBackground, contextBackground);
+   		parentForeground = getColor(rowForeground, gridForeground, contextForeground);
+   		parentFont = getFont(rowFont, gridFont, contextFont);
    		
    		// cell: load gc
    		cellBackground = cell.getBackground();
