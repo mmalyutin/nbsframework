c9537edb03fdd15f8c6fe126c2450a88ffe330a4
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-02-02 10:00:02 +0000

    Refactored

--- trunk/framework.report/src/org/plazmaforge/framework/report/export/AbstractBaseExporter.java
+++ trunk/framework.report/src/org/plazmaforge/framework/report/export/AbstractBaseExporter.java
@@ -22,7 +22,6 @@
 
 package org.plazmaforge.framework.report.export;
 
-import org.plazmaforge.framework.report.model.base.Pen;
 import org.plazmaforge.framework.uwt.graphics.Color;
 import org.plazmaforge.framework.uwt.graphics.Font;
 
@@ -33,19 +32,6 @@ import org.plazmaforge.framework.uwt.graphics.Font;
  */
 public abstract class AbstractBaseExporter extends AbstractReportExporter {
 
-    public static int DEFAULT_BORDER_WIDTH = 1;
-    
-    public static Color DEFAULT_BORDER_COLOR = Color.BLACK;
-    
-    public static Color DEFAULT_COLOR = Color.BLACK;
-    
-    public static String DEFAULT_FONT_NAME = "Hevletica";
-    
-    public static int DEFAULT_FONT_SIZE = 12;
-    
-    public static Font DEFAULT_FONT = new Font(DEFAULT_FONT_NAME, DEFAULT_FONT_SIZE);
-    
-    
     protected Color parentBackground;
     protected Color parentForeground;
     protected Font parentFont;
@@ -55,153 +41,12 @@ public abstract class AbstractBaseExporter extends AbstractReportExporter {
     protected Font font;
 
     
-    protected Color getColor(Color color, Color parentColor) {
-	//return color == null ? parentColor : color;
-	return getColor(color, parentColor, null);
-    }
-
-    protected Color getColor(Color color1, Color color2, Color color3) {
-	// find first not null color
-	if (color1 != null) {
-	    return color1;
-	}
-	if (color2 != null) {
-	    return color2;
-	}
-	return color3;
-    }
-
-    protected Font getFont(Font font, Font parentFont) {
-	/*
-	if (font == null && parentFont == null) {
-	    return null;
-	}
-	
-	if (font == null) {
-	    font = parentFont;
-	}
-	if (font.isComplete()) {
-	    return font;
-	}
-	
-	// merge incomplete font
-	String name = null;
-	int size = 0;
-	int style = 0;
-	
-	// font name
-	if (font.isEmptyName()) {
-	    if (parentFont != null && !parentFont.isEmptyName()) {
-		name = parentFont.getName();
-	    } else {
-		name = "";
-	    }
-	} else {
-	    name = font.getName();
-	}
-
-	// font size
-	if (font.isEmptySize()) {
-	    if (parentFont != null && !parentFont.isEmptySize()) {
-		size = parentFont.getSize();
-	    } else {
-		size = 12; // TODO
-	    }
-	} else {
-	    size = font.getSize();
-	}
-	
-	// font style
-	style = font.getStyle();
-	
-	return new Font(name , size, style);
-	*/
-	
-	return getFont(font, parentFont, null);
-    }
     
-    protected Font getFont(Font font1, Font font2, Font font3) {
-	// find first not null font
-	Font font = font1;
-	if (font == null) {
-	    font = font2;
-	}
-	if (font == null) {
-	    font = font3;
-	}
-	if (font == null) {
-	    return null;
-	}
-	if (font.isComplete()) {
-	    return font;
-	}
-	
-	// merge incomplete font
-	String name = null;
-	int size = 0;
-	int style = 0;
-	
-
-	// font name
-	name = getStrongFontName(font1);
-	if (name == null) {
-	    name = getStrongFontName(font2);
-	}
-	if (name == null) {
-	    name = getStrongFontName(font3);
-	}
-	if (name == null) {
-	    name = "";
-	}
-	
-	// font size
-	size = getStrongFontSize(font1);
-	if (size == 0) {
-	    size = getStrongFontSize(font2);
-	}
-	if (size == 0) {
-	    size = getStrongFontSize(font3);
-	}
-	if (size == 0) {
-	    size = DEFAULT_FONT_SIZE;
-	}
-	
-	// font style
-	style = font.getStyle();
-	
-	return new Font(name , size, style);
-    }
-    
-    protected String getStrongFontName(Font font) {
-	return (font == null || font.isEmptyName()) ? null : font.getName(); 
-    }
-
-    protected int getStrongFontSize(Font font) {
-	return font == null ? 0 : font.getSize(); 
-    }
-
     protected void normalizeCurrentStyle() {
 	background = getColor(background, parentBackground);
 	foreground = getColor(foreground, parentForeground);
 	font = getFont(font, parentFont);
     }
 
-    protected int getLineWidth(Pen pen) {
-	if (pen == null || pen.isEmpty()) {
-	    return 0;
-	}
-	int w = pen.getLineWidth();
-	if (w <= 0) {
-	    w = DEFAULT_BORDER_WIDTH;
-	}
-	return w;
-    }
-
-    protected Color getLineColor(Pen pen) {
-	if (pen == null) {
-	    return null;
-	}
-	return getColor(pen.getLineColor(), DEFAULT_BORDER_COLOR);
-    }
     
 }
--- trunk/framework.report/src/org/plazmaforge/framework/report/export/AbstractReportExporter.java
+++ trunk/framework.report/src/org/plazmaforge/framework/report/export/AbstractReportExporter.java
@@ -30,8 +30,11 @@ import java.util.Map;
 
 import org.plazmaforge.framework.core.data.formatter.FormatterManager;
 import org.plazmaforge.framework.core.data.formatter.STFormatterManager;
+import org.plazmaforge.framework.report.model.base.Pen;
 import org.plazmaforge.framework.report.model.base.grid.Cell;
 import org.plazmaforge.framework.util.StringUtils;
+import org.plazmaforge.framework.uwt.graphics.Color;
+import org.plazmaforge.framework.uwt.graphics.Font;
 
 
 /**
@@ -40,6 +43,20 @@ import org.plazmaforge.framework.util.StringUtils;
  */
 public abstract class AbstractReportExporter implements ReportExporter {
 
+    public static int DEFAULT_BORDER_WIDTH = 1;
+    
+    public static Color DEFAULT_BORDER_COLOR = Color.BLACK;
+    
+    public static Color DEFAULT_COLOR = Color.BLACK;
+    
+    public static String DEFAULT_FONT_NAME = "Hevletica";
+    
+    public static int DEFAULT_FONT_SIZE = 12;
+    
+    public static Font DEFAULT_FONT = new Font(DEFAULT_FONT_NAME, DEFAULT_FONT_SIZE);
+
+    
+    
     private FormatterManager formatterManager;
     
     private Map<String, Object> dataMap;
@@ -100,6 +117,148 @@ public abstract class AbstractReportExporter implements ReportExporter {
         return formatterManager;
     }
     
+    protected Color getColor(Color color, Color parentColor) {
+	//return color == null ? parentColor : color;
+	return getColor(color, parentColor, null);
+    }
+
+    protected Color getColor(Color color1, Color color2, Color color3) {
+	// find first not null color
+	if (color1 != null) {
+	    return color1;
+	}
+	if (color2 != null) {
+	    return color2;
+	}
+	return color3;
+    }
+
+    protected Font getFont(Font font, Font parentFont) {
+	/*
+	if (font == null && parentFont == null) {
+	    return null;
+	}
+	
+	if (font == null) {
+	    font = parentFont;
+	}
+	if (font.isComplete()) {
+	    return font;
+	}
+	
+	// merge incomplete font
+	String name = null;
+	int size = 0;
+	int style = 0;
+	
+	// font name
+	if (font.isEmptyName()) {
+	    if (parentFont != null && !parentFont.isEmptyName()) {
+		name = parentFont.getName();
+	    } else {
+		name = "";
+	    }
+	} else {
+	    name = font.getName();
+	}
+
+	// font size
+	if (font.isEmptySize()) {
+	    if (parentFont != null && !parentFont.isEmptySize()) {
+		size = parentFont.getSize();
+	    } else {
+		size = 12; // TODO
+	    }
+	} else {
+	    size = font.getSize();
+	}
+	
+	// font style
+	style = font.getStyle();
+	
+	return new Font(name , size, style);
+	*/
+	
+	return getFont(font, parentFont, null);
+    }
+    
+    protected Font getFont(Font font1, Font font2, Font font3) {
+	// find first not null font
+	Font font = font1;
+	if (font == null) {
+	    font = font2;
+	}
+	if (font == null) {
+	    font = font3;
+	}
+	if (font == null) {
+	    return null;
+	}
+	if (font.isComplete()) {
+	    return font;
+	}
+	
+	// merge incomplete font
+	String name = null;
+	int size = 0;
+	int style = 0;
+	
+
+	// font name
+	name = getStrongFontName(font1);
+	if (name == null) {
+	    name = getStrongFontName(font2);
+	}
+	if (name == null) {
+	    name = getStrongFontName(font3);
+	}
+	if (name == null) {
+	    name = "";
+	}
+	
+	// font size
+	size = getStrongFontSize(font1);
+	if (size == 0) {
+	    size = getStrongFontSize(font2);
+	}
+	if (size == 0) {
+	    size = getStrongFontSize(font3);
+	}
+	if (size == 0) {
+	    size = DEFAULT_FONT_SIZE;
+	}
+	
+	// font style
+	style = font.getStyle();
+	
+	return new Font(name , size, style);
+    }
+    
+    protected String getStrongFontName(Font font) {
+	return (font == null || font.isEmptyName()) ? null : font.getName(); 
+    }
+
+    protected int getStrongFontSize(Font font) {
+	return font == null ? 0 : font.getSize(); 
+    }
+
+    protected int getLineWidth(Pen pen) {
+	if (pen == null || pen.isEmpty()) {
+	    return 0;
+	}
+	int w = pen.getLineWidth();
+	if (w <= 0) {
+	    w = DEFAULT_BORDER_WIDTH;
+	}
+	return w;
+    }
+
+    protected Color getLineColor(Pen pen) {
+	if (pen == null) {
+	    return null;
+	}
+	return getColor(pen.getLineColor(), DEFAULT_BORDER_COLOR);
+    }
     
 }
 
