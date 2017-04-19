2181ec36f99355a57f76e30fed96b899a02fee0c
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-04-19 13:07:23 +0000

    Add processing ResultSet

--- framework.core/src/org/plazmaforge/framework/core/datastorage/DSDataHelper.java
+++ framework.core/src/org/plazmaforge/framework/core/datastorage/DSDataHelper.java
@@ -23,14 +23,17 @@
 package org.plazmaforge.framework.core.datastorage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.plazmaforge.framework.core.criteria.Operation;
 import org.plazmaforge.framework.core.datastorage.data.QueryAnalyzer;
 import org.plazmaforge.framework.core.datastorage.data.QueryInfo;
 import org.plazmaforge.framework.core.datastorage.data.QueryParameter;
 import org.plazmaforge.framework.core.exception.DSEvaluateException;
+import org.plazmaforge.framework.core.exception.DSException;
 import org.plazmaforge.framework.util.StringUtils;
 
 /**
@@ -271,6 +274,207 @@ public class DSDataHelper {
 	    return null;
 	}
 	return  expressionEvaluator.evaluate(expression);
+    }
+    
+    public DSResultSet processResultSet(DSResultSet resultSet, DSDataSource dataSource) throws DSException {
+	if (resultSet == null) {
+	    return null;
+	}
+	if (resultSet.isEmpty() || resultSet.isClosed()) {
+	    return resultSet;
+	}
+	
+	if (!needProcessing(dataSource)) {
+	    return resultSet;
+	}
+	
+	List<DSField> fields = dataSource.getFields();
+	List<DSFilter> filters = dataSource.getFilters();
+	
+	Map<String, Integer> fieldIndexMap = createFieldIndexMap(fields);
+	DSArrayResultSet result = new DSArrayResultSet();
+	List<Object[]> records = new ArrayList<Object[]>();
+	Object[] record = null;
+	
+	// Load all data to DSArrayResultSet
+	while (resultSet.next()) {
+	    
+	    // Get record
+	    record = getRecord(resultSet, fields);
+	    
+	    // Apply filters
+	    if (isFilter(record, fieldIndexMap, filters)) {
+		records.add(record);
+	    }
+	}
+	
+	result.setData(records);
+	
+	//TODO: orders
+	return result;
+    }
+    
+    protected Map<String, Integer> createFieldIndexMap(List<DSField> fields) {
+	Map<String, Integer> fieldIndexMap = new HashMap<String, Integer>();
+	if (fields == null || fields.isEmpty()) {
+	    return fieldIndexMap;
+	}
+	int size = fields.size();
+	for (int i = 0; i < size; i++) {
+	    fieldIndexMap.put(fields.get(i).getName(), i);
+	}
+	return fieldIndexMap;
+    }
+    
+    protected Object[] getRecord(DSResultSet resultSet, List<DSField> fields) throws DSException {
+	if (resultSet == null || fields == null) {
+	    return null;
+	}
+	int size = fields.size();
+	Object[] record = new Object[size];
+	for (int i = 0; i < size; i++) {
+	    record[i] = resultSet.getValue(fields.get(i).getName());
+	}
+	return record;
+    }
+    
+    protected boolean isFilter(Object[] record, Map<String, Integer> fieldIndexMap, List<DSFilter> filters) {
+	if (filters == null || filters.isEmpty()) {
+	    return true;
+	}
+	for (DSFilter filter : filters) {
+	    if (!isFilter(record, fieldIndexMap, filter)) {
+		return false;
+	    }
+	}
+	return true;
+    }
+    
+    protected boolean isFilter(Object[] record, Map<String, Integer> fieldIndexMap, DSFilter filter) {
+	if (filter == null) {
+	    return true;
+	}
+	
+	// TODO: Implemented only for DSFieldFilter
+	if (filter instanceof DSFieldFilter) {
+	    DSFieldFilter fieldFilter = ((DSFieldFilter) filter);
+	    DSField field = fieldFilter.getField();
+	    if (field == null) {
+		return true;
+	    }
+	    String fieldName = field.getName();
+	    if (fieldName == null) {
+		return true;
+	    }
+	    Integer index = fieldIndexMap.get(fieldName);
+	    if (index == null) {
+		return true;
+	    }
+	    if (index < 0 || index > record.length - 1) {
+		return true;
+	    }
+	    Object leftValue = record[index];
+	    Object rightValue = fieldFilter.getValue();
+	    String operation = fieldFilter.getOperation();
+	    return isFilterByOperation(leftValue, operation, rightValue);
+	}
+	
+	//TODO
+	return true;
+    }
+    
+    /*
+    protected Boolean evaluateBooleanValue(Object leftValue, String operation, Object rightValue) {
+	if (operation == null) {
+	    operation = Operation.EQ;
+	}
+    }
+    */
+    
+    protected boolean isFilterByOperation(Object leftValue, String operation, Object rightValue) {
+	if (operation == null) {
+	    operation = Operation.EQ;
+	}
+
+	if (Operation.EQ.equals(operation)) {
+	    return isEQ(leftValue, rightValue);
+	}
+	if (Operation.NE.equals(operation)) {
+	    return isNE(leftValue, rightValue);
+	}
+	if (Operation.LIKE.equals(operation)) {
+	    return isLIKE(leftValue, rightValue);
+	}
+	if (Operation.NOTLIKE.equals(operation)) {
+	    return isNOTLIKE(leftValue, rightValue);
+	}
+	if (Operation.LT.equals(operation)) {
+	    return isLT(leftValue, rightValue);
+	}
+	if (Operation.LE.equals(operation)) {
+	    return isLE(leftValue, rightValue);
+	}
+	if (Operation.GT.equals(operation)) {
+	    return isGT(leftValue, rightValue);
+	}
+	if (Operation.GE.equals(operation)) {
+	    return isGE(leftValue, rightValue);
+	}
+
+	// Default
+	return isEQ(leftValue, rightValue);
+    }    
+    
+    protected boolean isEQ(Object leftValue, Object rightValue) {
+	if (leftValue == null || rightValue == null) {
+	    return false;
+	}
+	return leftValue.equals(rightValue);
+    }
+
+    protected boolean isNE(Object leftValue, Object rightValue) {
+	return !isEQ(leftValue, rightValue);
+    }
+
+    protected boolean isLIKE(Object leftValue, Object rightValue) {
+	//TODO
+	return true;
+    }
+
+    protected boolean isNOTLIKE(Object leftValue, Object rightValue) {
+	return !isLIKE(leftValue, rightValue);
+    }
+
+    protected boolean isLT(Object leftValue, Object rightValue) {
+	//TODO
+	return true;
+    }
+
+    protected boolean isLE(Object leftValue, Object rightValue) {
+	//TODO
+	return true;
+    }
+
+    protected boolean isGT(Object leftValue, Object rightValue) {
+	//TODO
+	return true;
+    }
+    
+    protected boolean isGE(Object leftValue, Object rightValue) {
+	//TODO
+	return true;
+    }
+    
+    
+    ////
+    
+    
+    public boolean needProcessing(DSDataSource dataSource) {
+	if (dataSource == null || !dataSource.hasFields()) {
+	    return false;
+	}
+	// Only for filter or order
+	return dataSource.hasFilters() || dataSource.hasOrders();
     }
     
     protected String normalize(String str) {
