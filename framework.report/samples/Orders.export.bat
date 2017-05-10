7ba1a606d5c1e88c4055c040f69a4c30ed6bec6e
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-05-10 09:44:30 +0000

    Add new samples

---
+++ framework.report/samples/Orders.export.bat
@@ -0,0 +1,2 @@
+cd..
+runExport.bat -input-file samples/reports/Orders.document.xml -output-format "html" -log true
---
+++ framework.report/samples/Orders.pdf.bat
@@ -0,0 +1,2 @@
+cd..
+runReport.bat -report-file samples/reports/Orders.report.xml -output-format "pdf" -connection "csv::samples/data/orders.csv->(firstRowHeader=true;rowDelimiter=LF;encoding=utf-8)" -log true
