18ca51a1e292c9473cba732d1f5e08cf0d3588fb
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-05-30 12:41:55 +0000

    Add new XML writers

--- trunk/framework.report/src/org/plazmaforge/framework/report/storage/xml/report/XMLGroupWriter.java
+++ trunk/framework.report/src/org/plazmaforge/framework/report/storage/xml/report/XMLGroupWriter.java
@@ -25,7 +25,10 @@
  */
 package org.plazmaforge.framework.report.storage.xml.report;
 
+import java.util.List;
+
 import org.jdom.Element;
+import org.plazmaforge.framework.report.model.design.Band;
 import org.plazmaforge.framework.report.model.design.ReportGroup;
 
 
@@ -41,7 +44,51 @@ import org.plazmaforge.framework.report.model.design.ReportGroup;
 public class XMLGroupWriter extends XMLAbstractReportWriter {
 
     public void writeGroup(ReportGroup group, Element node) {
-	//TODO
+	writeGroupAttributes(group, node);
+	writeGroupContent(group, node);
     }
     
+    protected void writeGroupAttributes(ReportGroup template, Element node) {
+	
+   	// name
+   	if (template.getName() != null) {
+   	    setStringValue(node, XML_ATTR_NAME, template.getName());
+   	}
+    }
+    
+    protected void writeGroupContent(ReportGroup template, Element node) {
+	
+	// expression
+	if (template.hasExpressionText()) {
+	    Element expressionNode = createElement(XML_EXPRESSION);
+	    setExpression(template.getExpression(), expressionNode, USE_DATA_TYPE_IN_EXPRESSION);
+	}
+	
+	writeBands(template, node);
+    }  
+    
+    // BANDS
+    protected void writeBands(ReportGroup template, Element node) {
+	Element childNode = buildBandsNode(template);
+	if (childNode == null) {
+	    return;
+	}
+	addChild(node, childNode);	
+    }
+
+    protected Element buildBandsNode(ReportGroup template) {
+   	if (!template.hasBands()) {
+   	    return null;
+   	}
+   	List<Band> bands = template.getBands();
+   	Element parentNode = createElement(XML_BANDS);
+   	Element childNode = null;
+   	XMLBandWriter writer = new XMLBandWriter();
+   	for (Band band : bands) {
+   	    childNode = createElement(XML_BAND);
+   	    writer.writeBand(band, childNode);
+   	    addChild(parentNode, childNode);
+   	}
+   	return parentNode;
+   }    
 }
