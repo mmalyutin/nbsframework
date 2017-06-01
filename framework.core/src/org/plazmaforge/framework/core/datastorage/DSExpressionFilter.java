22f4b26429270961f8794e4118564bb98d1ff7c8
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-06-01 08:40:17 +0000

    Refactored

--- framework.core/src/org/plazmaforge/framework/core/datastorage/DSExpressionFilter.java
+++ framework.core/src/org/plazmaforge/framework/core/datastorage/DSExpressionFilter.java
@@ -63,6 +63,10 @@ public class DSExpressionFilter extends DSFilter implements HasExpression {
 	getExpression().setText(text);
     }
 
+    public boolean hasExpressionText() {
+  	return DSExpression.hasText(expression);
+    }
+    
     @Override
     public String toString() {
 	return "DSExpressionFilter[expression=" + expression + "]";
