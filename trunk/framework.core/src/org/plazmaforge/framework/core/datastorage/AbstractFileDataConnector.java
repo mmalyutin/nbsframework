85835e0d80593d93a1509aafeb493342500b510b
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-06-02 12:24:37 +0000

    Add equals/hashCode

--- trunk/framework.core/src/org/plazmaforge/framework/core/datastorage/AbstractFileDataConnector.java
+++ trunk/framework.core/src/org/plazmaforge/framework/core/datastorage/AbstractFileDataConnector.java
@@ -68,6 +68,44 @@ public abstract class AbstractFileDataConnector extends AbstractDataConnector {
         this.encoding = encoding;
     }
 
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result
+		+ ((encoding == null) ? 0 : encoding.hashCode());
+	result = prime * result + ((file == null) ? 0 : file.hashCode());
+	result = prime * result + ((folder == null) ? 0 : folder.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	AbstractFileDataConnector other = (AbstractFileDataConnector) obj;
+	if (encoding == null) {
+	    if (other.encoding != null)
+		return false;
+	} else if (!encoding.equals(other.encoding))
+	    return false;
+	if (file == null) {
+	    if (other.file != null)
+		return false;
+	} else if (!file.equals(other.file))
+	    return false;
+	if (folder == null) {
+	    if (other.folder != null)
+		return false;
+	} else if (!folder.equals(other.folder))
+	    return false;
+	return true;
+    }
+
   
     
 }
--- trunk/framework.core/src/org/plazmaforge/framework/datastorage/support/sql/SQLDataConnector.java
+++ trunk/framework.core/src/org/plazmaforge/framework/datastorage/support/sql/SQLDataConnector.java
@@ -66,6 +66,37 @@ public class SQLDataConnector extends AbstractDataConnector {
     public String getType() {
 	return TYPE;
     }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result + ((driver == null) ? 0 : driver.hashCode());
+	result = prime * result + ((url == null) ? 0 : url.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	SQLDataConnector other = (SQLDataConnector) obj;
+	if (driver == null) {
+	    if (other.driver != null)
+		return false;
+	} else if (!driver.equals(other.driver))
+	    return false;
+	if (url == null) {
+	    if (other.url != null)
+		return false;
+	} else if (!url.equals(other.url))
+	    return false;
+	return true;
+    }
     
     
 
