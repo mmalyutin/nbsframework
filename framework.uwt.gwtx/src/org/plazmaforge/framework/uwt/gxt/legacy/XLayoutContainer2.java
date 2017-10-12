e60dcf250521650760bce1a554662d859cdf45f8
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2017-10-12 06:51:03 +0000

    Refactored

--- framework.uwt.gwtx/src/org/plazmaforge/framework/uwt/gxt/legacy/XLayoutContainer2.java
+++ framework.uwt.gwtx/src/org/plazmaforge/framework/uwt/gxt/legacy/XLayoutContainer2.java
@@ -20,7 +20,7 @@
  * ohapon@users.sourceforge.net
  */
 
-package org.plazmaforge.framework.uwt.gxt.widget;
+package org.plazmaforge.framework.uwt.gxt.legacy;
 
 import java.util.ArrayList;
 import java.util.Iterator;
