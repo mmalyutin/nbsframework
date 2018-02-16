9a63810380fbd8a8a6e3f230f78df350dd034938
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2018-02-16 08:48:36 +0000

    Refactored application model

---
+++ framework.uwt.swt/src/org/plazmaforge/framework/uwt/swt/MyApplication.java
@@ -0,0 +1,42 @@
+package org.plazmaforge.framework.uwt.swt;
+
+import org.plazmaforge.framework.uwt.Application;
+import org.plazmaforge.framework.uwt.UWTDesktopToolit;
+import org.plazmaforge.framework.uwt.event.SelectionEvent;
+import org.plazmaforge.framework.uwt.event.SelectionListener;
+import org.plazmaforge.framework.uwt.widget.Button;
+import org.plazmaforge.framework.uwt.widget.Frame;
+import org.plazmaforge.framework.uwt.widget.Label;
+
+public class MyApplication extends Application {
+
+    public static void main(String[] args) {
+	
+	UWTDesktopToolit.initUWT("swt");
+	
+	// Create application
+	MyApplication application = new MyApplication();
+
+	
+	Frame frame = application.getFrame();
+	frame.setTitle("My Application");
+	
+	final Label label = new Label("Hello World!");
+	frame.add(label);
+	
+	Button button = new Button("Press Me");
+	button.addSelectionListener(new SelectionListener() {
+	    
+	    @Override
+	    public void select(SelectionEvent event) {
+		label.setText("Hi all!");
+		
+	    }
+	});
+	
+	frame.add(button);
+	
+	// Start application
+	application.start();
+    }
+}
