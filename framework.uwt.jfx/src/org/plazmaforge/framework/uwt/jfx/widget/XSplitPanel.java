6caa4789b845de99fece9e59bb543959d2e2c699
ohapon <ohapon@d4634b0a-978f-4fb5-935f-1a567c2cf0d6>
2018-04-11 11:19:35 +0000

    Add SplitPanel adapter

---
+++ framework.uwt.jfx/src/org/plazmaforge/framework/uwt/jfx/widget/XSplitPanel.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2012-2013 Oleh Hapon ohapon@users.sourceforge.net
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * Oleh Hapon
+ * Kyiv, UKRAINE
+ * ohapon@users.sourceforge.net
+ *
+ */
+package org.plazmaforge.framework.uwt.jfx.widget;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.SplitPane;
+
+/**
+ * 
+ * @author ohapon
+ *
+ */
+public class XSplitPanel extends SplitPane implements XContainer {
+
+    public XSplitPanel() {
+	super();
+    }
+
+    protected ObservableList<Node> doGetChildren() {
+	return getChildren();
+    }
+    
+    @Override
+    public void addChild(Node node) {
+  	doGetChildren().add(node);
+    }
+    
+    @Override
+    public void removeChild(Node node) {
+  	doGetChildren().remove(node);
+    }    
+    
+    @Override
+    public void removeAll() {
+        getChildren().clear();
+    }
+ 
+    @Override
+    public boolean hasChildren() {
+        return !getChildren().isEmpty();
+    }
+ 
+    @Override
+    public int getChildrenCount() {
+        return getChildren().size();
+    }
+ 
+    @Override
+    public Node getChild(int index) {
+        return getChildren().get(index);
+    }
+
+    ////
+        
+
+}
